8c32f3399d98bc08dd0b6e85993bc8c0a5ab5cfc
Dudu <aeiou7352@gmail.com>
2024-02-24 23:58:59 +0100

    basic game connection for frontend

--- static/script.js
+++ static/script.js
@@ -1,29 +1,48 @@
 const websocketConnection = new WebSocket("ws://127.0.0.1:8080/api/ws");
 
-        function startPlaying() {
-            // TODO
-            console.log("Starting game...");
-        }
+function startPlaying() {
+  // TODO
+  console.log("Starting game...");
+  websocketConnection.send(JSON.stringify({
+    startGame: {
+      publicGame: true,
+    },
+  }));
+}
 
-        function startPrivate() {
-            // TODO
-        }
+function startPrivate() {
+  // TODO
+  console.log("Starting private...");
+  websocketConnection.send(JSON.stringify({
+    startGame: {
+      publicGame: false,
+    },
+  }));
+}
 
-        /**
-         * @param {SubmitEvent} e
-         */
-        function joinPriv(e) {
-            e.preventDefault();
-            // TODO
-        }
-        
-        websocketConnection.addEventListener("open" , e => {
-            console.log("Websocket connected", e);
-        })
+/**
+ * @param {SubmitEvent} e
+ */
+function joinPriv(e) {
+  e.preventDefault();
+  // TODO
+  console.log(e.target.elements["id"].value);
+  websocketConnection.send(JSON.stringify({
+    joinPrivGame: {
+      gameId: e.target.elements["id"].value,
+    },
+  }));
+}
 
-        websocketConnection.addEventListener("message", messageEvent => {
-            // TODO
-        })
+websocketConnection.addEventListener("open", (e) => {
+  console.log("Websocket connected", e);
+});
 
-        document.getElementById("start-game").addEventListener("click", startPlaying);
-        document.getElementById("join-priv").addEventListener("submit", joinPriv)
+websocketConnection.addEventListener("message", (messageEvent) => {
+  // TODO
+  console.log(messageEvent.data);
+});
+
+document.getElementById("start-game").addEventListener("click", startPlaying);
+document.getElementById("start-priv").addEventListener("click", startPrivate);
+document.getElementById("join-priv").addEventListener("submit", joinPriv);
