c47f4b4328e28d3b1fe7edbbf219bc2e45aea84c
Dudu <aeiou7352@gmail.com>
2024-02-25 21:18:11 +0100

    serialize server messages to camelCase

--- src/messages/server.rs
+++ src/messages/server.rs
@@ -3,6 +3,7 @@ use serde::Serialize;
 use uuid::Uuid;
 
 #[derive(Message, Serialize)]
+#[serde(rename_all = "camelCase")]
 #[rtype(result = "()")]
 pub enum ServerGameEvent {
     GameWaiting(GameWaiting),
@@ -12,22 +13,26 @@ pub enum ServerGameEvent {
 }
 
 #[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct GameWaiting {
     pub game_id: Uuid
 }
 
 #[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct GameStarted {
     pub game_id: Uuid,
     pub your_turn: bool,
 }
 
 #[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct GameEnded {
     pub won: bool,
 }
 
 #[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct OpponentMove {
     pub x: usize,
     pub y: usize,
