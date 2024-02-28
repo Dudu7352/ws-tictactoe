1d78afad70030b28ba61a69ce3772af0c387a134
Dudu <aeiou7352@gmail.com>
2024-02-28 19:26:24 +0100

    fix get_winner method

--- src/game.rs
+++ src/game.rs
@@ -25,26 +25,34 @@ impl Game {
         match self {
             Game::Waiting { player_id: _ } => None,
             Game::Started { players, board, first_player_turn: _ } => {
-                for player_id in [0, 1] {
+                for player_id in [0usize, 1usize] {
+                    let mut diag_up = 0;
+                    let mut diag_down = 0;
+                    let mut col: u8 = 0;
+                    let mut row: u8 = 0;
                     for y in 0..3usize {
-                        let mut col: u8 = 0;
-                        let mut row: u8 = 0;
-                        let mut diag_up: u8 = 0;
-                        let mut diag_down: u8 = 0;
+                        col = 0;
+                        row = 0;
+                        diag_up += (board[y][y] == player_id as i8) as u8;
+                        diag_down += (board[y][board.len() - y - 1] == player_id as i8) as u8;
+                        
                         for x in 0..3usize {
-                            if board[x][y] == player_id as i8 && x == y { diag_down += 1; }
-                            if board[x][y] == player_id as i8 && x == 2-y { diag_up += 1; }
-                            if board[y][x] == player_id as i8 { row += 1; }
-                            if board[x][y] == player_id as i8 { col += 1; }
+                            col += (board[x][y] == player_id as i8) as u8;
+                            row += (board[y][x] == player_id as i8) as u8;
                         }
-                        if col==3 || row==3 || diag_up==3 || diag_down==3 {
-                            return Some(
-                                GameEndResults {
-                                    winner: players[player_id],
-                                    loser: players[1-player_id]
-                                }
-                            );
+
+                        if col==3 || row==3 {
+                            break;
                         }
+                    }
+
+                    if col==3 || row==3 || diag_up==3 || diag_down==3 {
+                        return Some(
+                            GameEndResults {
+                                winner: players[player_id],
+                                loser: players[1-player_id]
+                            }
+                        );
                     }
                 }
                 None
