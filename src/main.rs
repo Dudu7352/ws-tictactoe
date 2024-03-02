b3ffa3f127931c213552b1eb95ca99dfcde5373a
Dudu <aeiou7352@gmail.com>
2024-03-02 22:39:15 +0100

    reroute /static to /

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ mod game_service;
 
 use actix::{Actor, Addr};
 use actix_web::{
-    get,
-    web::{self, Data},
-    App, Error, HttpRequest, HttpResponse, HttpServer, Scope,
+    get, web::{self, Data}, App, Error, HttpRequest, HttpResponse, HttpServer, Scope
 };
 use actix_web_actors::ws;
 use game_service::GameService;
@@ -16,7 +14,6 @@ use crate::client_conn::ClientConn;
 
 #[get("/ws")]
 async fn connect_ws(req: HttpRequest, stream: web::Payload, data: Data<Addr<GameService>>) -> Result<HttpResponse, Error> {
-    println!("Websocket connection initialization");
     ws::start(ClientConn::new(data.as_ref().clone()), &req, stream)
 }
 
@@ -25,11 +22,10 @@ async fn main() -> std::io::Result<()> {
     let game_service = GameService::new().start();
 
     HttpServer::new(move || {
-        println!("Created game service");
         App::new()
             .app_data(Data::new(game_service.clone()))
-            .service(actix_files::Files::new("static", "./static/").show_files_listing())
             .service(Scope::new("/api").service(connect_ws))
+            .service(actix_files::Files::new("/", "./static").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .run()
